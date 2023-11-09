6b946365191a58b0a9ff9d91ed2c7cdc32b8cad5
Dylan D Shelton <dshelt@umich.edu>
2023-11-09 13:41:52 -0500

    LoggedIn Missing code updates

--- Client/RoomieMatter/Sources/Screens/LoggedIn/Chore.swift
+++ Client/RoomieMatter/Sources/Screens/LoggedIn/Chore.swift
@@ -1,8 +1,13 @@
-//
-//  Chore.swift
-//  RoomieMatter
-//
-//  Created by Dylan Shelton on 11/7/23.
-//
+
 
 import Foundation
+
+struct Chore{
+    let name: String
+    let date: Double
+    let discription: String
+    var assignedRoommates: [Roommate]
+    
+    static let Example1 = Chore(name: "Clean Living Room", date: Date().timeIntervalSince1970, discription: "Vacuum and mop living room.", assignedRoommates: [Roommate.Example1])
+    static let Example2 = Chore(name: "Prepare Thanksgiving Dinner", date: Date().timeIntervalSince1970 + 86400, discription: "This Thursday we will have a small dinner with some friends. Could you make it?", assignedRoommates: [Roommate.Example3, Roommate.Example4])
+}
