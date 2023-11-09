ca3b85f052dc7eac85c23d73e75677f96b10aab1
Dylan D Shelton <dshelt@umich.edu>
2023-11-09 15:42:42 -0500

    Update roommate and Chore structs

--- Client/RoomieMatter/Sources/Screens/LoggedIn/Chore.swift
+++ Client/RoomieMatter/Sources/Screens/LoggedIn/Chore.swift
@@ -2,12 +2,14 @@
 
 import Foundation
 
-struct Chore{
-    let name: String
-    let date: Double
-    let discription: String
+struct Chore: Identifiable{
+    let id: String
+    var name: String
+    var date: Double
+    var description: String
+    var author: Roommate
     var assignedRoommates: [Roommate]
     
-    static let Example1 = Chore(name: "Clean Living Room", date: Date().timeIntervalSince1970, discription: "Vacuum and mop living room.", assignedRoommates: [Roommate.Example1])
-    static let Example2 = Chore(name: "Prepare Thanksgiving Dinner", date: Date().timeIntervalSince1970 + 86400, discription: "This Thursday we will have a small dinner with some friends. Could you make it?", assignedRoommates: [Roommate.Example3, Roommate.Example4])
+    static let Example1 = Chore(id: "1", name: "Clean Living Room", date: Date().timeIntervalSince1970, description: "Vacuum and mop living room.", author: Roommate.Example2, assignedRoommates: [Roommate.Example1])
+    static let Example2 = Chore(id: "2", name: "Prepare Thanksgiving Dinner", date: Date().timeIntervalSince1970 + 86400, description: "This Thursday we will have a small dinner with some friends. Could you make it?", author: Roommate.Example2, assignedRoommates: [Roommate.Example3, Roommate.Example2, Roommate.Example4])
 }
