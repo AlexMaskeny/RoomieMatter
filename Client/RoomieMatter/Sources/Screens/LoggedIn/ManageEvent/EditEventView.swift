603abac330354825f608c914207273ad2ac6c8d9
Dylan D Shelton <dshelt@umich.edu>
2023-11-28 13:14:53 -0500

    added event edit view

---
+++ Client/RoomieMatter/Sources/Screens/LoggedIn/ManageEvent/EditEventView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventView.swift
+//  RoomieMatter
+//
+//  Created by Dylan Shelton on 11/28/23.
+//
+
+import SwiftUI
+
+struct EditEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEventView()
+}
---
+++ Client/RoomieMatter/Sources/Screens/LoggedIn/ManageEvent/EditEventViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditEventViewViewModel.swift
+//  RoomieMatter
+//
+//  Created by Dylan Shelton on 11/28/23.
+//
+
+import Foundation
