4cd2ccf8e0e6a81a0ceef255630d446794d89dbe
tkoli <tkoli@umich.edu>
2023-12-03 16:09:37 -0500

    test create room

--- Client/RoomieMatter/Sources/Screens/LoggedIn/RoomHomeView.swift
+++ Client/RoomieMatter/Sources/Screens/LoggedIn/RoomHomeView.swift
@@ -2,9 +2,52 @@
 //  RoomHomeView.swift
 //  RoomieMatter
 //
-//  Created by Tanuj Koli on 11/30/23.
+//  LASYA
 //
 
 import Foundation
 
 //initial commit
+import SwiftUI
+
+struct CreateRoomView: View {
+    @State private var creatorName: String = ""
+    @State private var roomName: String = ""
+    @State private var showShareSheet: Bool = false
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Room Information")) {
+                    TextField("Creator Name", text: $creatorName)
+                    TextField("Room Name", text: $roomName)
+                }
+                Button(action: {
+                    print("Room Created")
+                    self.showShareSheet = true
+                }) {
+                    Text("Create Room")
+                }
+            }
+            .navigationBarTitle("Create a New Room")
+            .sheet(isPresented: $showShareSheet) {
+                ShareSheet(activityItems: ["Join my room: \(roomName) on RoomieMatter!"])
+            }
+        }
+    }
+}
+
+struct ShareSheet: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+    }
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
+
+struct CreateRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateRoomView()
+    }
+}
+
