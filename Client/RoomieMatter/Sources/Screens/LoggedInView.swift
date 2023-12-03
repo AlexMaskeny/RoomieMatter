d180b3e5cd9b5101cb29d109aad3aeb32e2e98ca
lasyamantha <67132326+lasyamantha@users.noreply.github.com>
2023-12-03 17:19:30 -0500

    added join link button (copy to clipboard)

--- Client/RoomieMatter/Sources/Screens/LoggedInView.swift
+++ Client/RoomieMatter/Sources/Screens/LoggedInView.swift
@@ -3,9 +3,16 @@ import FirebaseAuth
 import FirebaseFirestore
 import Firebase
 import SwiftUI
+import UIKit
+
+func copyToClipboard(text: String) {
+    let pasteboard = UIPasteboard.general
+    pasteboard.string = text
+}
 
 struct LoggedInView: View {
     @StateObject var viewModel = LoggedInViewViewModel()
+    @State private var showingAlert = false
     var body: some View {
         NavigationStack{
             TabView {
@@ -33,13 +40,32 @@ struct LoggedInView: View {
                     
                 }
                 
+                
                 ToolbarItemGroup(placement: .topBarTrailing) {
-                    NavigationLink {
-                        SyncCalendarView()
-                    } label: {
-                        Image(systemName: "calendar")
-                            .foregroundStyle(.white)
-                    }
+                    
+//                    Button(action: {
+//                                copyToClipboard(text: "Join my room: \(viewModel.roomName) on RoomieMatter!")
+//                            }) {
+//                                Image(systemName: "clipboard")
+//                                    .foregroundStyle(.white)
+//                            }
+                    Button(action: {
+                                            copyToClipboard(text: "Join my room: \(viewModel.roomName) on RoomieMatter!")
+                                            self.showingAlert = true
+                                        }) {
+                                            Image(systemName: "clipboard")
+                                                .foregroundStyle(.white)
+                                        }
+                                        .alert(isPresented: $showingAlert) {
+                                            Alert(title: Text("Copied"), message: Text("Join room info has been copied to clipboard"), dismissButton: .default(Text("Got it!")))
+                                        }
+//                    NavigationLink {
+//                        SyncCalendarView()
+//                    } label: {
+//                        Image(systemName: "calendar")
+//                            .foregroundStyle(.white)
+//                    }
+                    
                     NavigationLink {
                         ChatScreen()
                     } label: {
@@ -55,6 +81,3 @@ struct LoggedInView: View {
         .environmentObject(viewModel)
     }
 }
-
-
-
