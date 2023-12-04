5f51fff3885626bdcf06cdaa2b8de3d8e93513db
tkoli <tkoli@umich.edu>
2023-12-03 19:43:20 -0500

    roomHome split

--- Client/RoomieMatter/Sources/Screens/LoggedIn/RoomHome.swift
+++ Client/RoomieMatter/Sources/Screens/LoggedIn/RoomHome.swift
@@ -14,20 +14,32 @@ struct RoomHome: View {
             // Banner with RoomieMatter text, notification bell, and profile icons
             HStack {
                 Spacer()
-                Image(systemName: "bell")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.trailing, 10)
-                Image(systemName: "person.circle")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.trailing, 16)
+                Button(action: {
+                    // Handle navigation to notifications
+                    print("Notifications tapped")
+                }) {
+                    Image(systemName: "bell")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 10)
+                }
+                
+                Button(action: {
+                    // Handle navigation to profile
+                    print("Profile tapped")
+                }) {
+                    Image(systemName: "person.circle")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.trailing, 16)
+                }
+                
             }
             .padding()
             .background(Color.roomieMatter)
             
             // Your Rooms label
-            Text("Your Rooms")
+            Text("Rooms")
                 .font(.title)
                 .fontWeight(.bold)
                 .padding(.top, 16)
@@ -49,18 +61,31 @@ struct RoomHome: View {
             
             Spacer()
             
-            // Create Room button as a rounded rectangle
-            Button(action: {
-                // Handle the action for creating a room
-                print("Create Room tapped")
-            }) {
-                Text("Create Room")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.roomieMatter))
+            HStack {
+                // Create Room button as a rounded rectangle
+                Button(action: {
+                    // Handle the action for creating a room
+                    print("Join Room tapped")
+                }) {
+                    Text(" Join Room ")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.roomieMatter))
+                }
+                .padding()
+                Button(action: {
+                    // Handle the action for creating a room
+                    print("Create Room tapped")
+                }) {
+                    Text("Create Room")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.roomieMatter))
+                }
             }
-            .padding()
+            
         }
         .edgesIgnoringSafeArea(.all)
     }
